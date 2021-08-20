68c50957a76cc599696521b1018ccf8ea1e45ade
valliant71 <valliant71@yahoo.com>
2021-08-20 12:18:16 -1000

    added Logo and react-tilt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import Navigation from './components/navigation/navigation';
+import Logo from './components/Logo/Logo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Logo /> */}
+       <Logo />
       {/* <ImageLinkForm/> */}
       {/* <FaceRecognition />} */}
     </div>
