2d1358ee0d8758cf8cdb1f2bb0f8685335059f0b
valliant71 <valliant71@yahoo.com>
2021-08-20 12:05:57 -1000

    added navigation and tachyons

---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+    <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <p>Sign Out</p>
+    </nav>
+    );
+}
+
+
+export default Navigation;
