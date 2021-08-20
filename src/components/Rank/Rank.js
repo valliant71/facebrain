02a87285dfc354a0c810be4fced730724393c59e
valliant71 <valliant71@yahoo.com>
2021-08-20 13:28:13 -1000

    added particles npm

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,6 +3,12 @@ import React from 'react';
 const Rank = () => {
     return (
     <div>
+        <div className='white f3'>
+            {'Bruce, your current rank is...'}
+        </div>
+        <div className='white f1'>
+            {'#1'}
+        </div>
 
     </div>
     );
