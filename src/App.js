35159b8a144be0433cb29bd081d2cd2549c6cd14
valliant71 <valliant71@yahoo.com>
2021-08-20 15:20:19 -1000

    fixed particles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+
+const particleOptions = {
+  particles: {
+    number: {
+      value:100,
+      density: {
+        enable:true,
+        value_area:800
+      }
+    }
+    }
+  }
+
 function App() {
   return (
-    <div className="App">
+    <div id='tsparticles' className="App">
       <Navigation />
+      <Particles id='tsparticles' className='particles'
+        params={particleOptions}
+      />
        <Logo />
        <Rank />
       <ImageLinkForm/>
@@ -16,5 +33,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
